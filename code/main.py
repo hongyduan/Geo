db77f9342fcce68a8ae1312c0ac8a6c2ceec6a7f
hongyduan <2042370010@qq.com>
2019-11-11 18:11:02 +0800

    add Hit@3 and MRR

--- code/main.py
+++ code/main.py
@@ -30,8 +30,8 @@ def parse_args(args=None):
     parser.add_argument('--leaf_node_entity', type=int, default=1)
     parser.add_argument('--cuda', type=int, default=0)
     parser.add_argument('--init_checkpoint', type=int, default=0)
-    parser.add_argument('--specific_test', type=int, default=1)
-    parser.add_argument('--specific_train', type=int, default=1)
+    parser.add_argument('--specific_test', type=int, default=0)
+    parser.add_argument('--specific_train', type=int, default=0)
 
     parser.add_argument('--relu_use_rgcn_layer1', type=int, default=1)
     parser.add_argument('--relu_use_concept_layer', type=int, default=1)
@@ -52,7 +52,7 @@ def parse_args(args=None):
     parser.add_argument('--final_dim', type=int, default=200)
     parser.add_argument('--num_bases', type=int, default=30)
     parser.add_argument('--num_classes', type=int, default=106)
-    parser.add_argument('--epoch', type=int, default=3)
+    parser.add_argument('--epoch', type=int, default=2)
     parser.add_argument('--sample_num', type=int, default=150)
 
     return parser.parse_args(args)
@@ -74,12 +74,16 @@ def set_logger(args):
     logging.getLogger('').addHandler(console)
 
 
-def save_model(model, optimizer, args, biggest_score):
+def save_model(model, optimizer, args, biggest_score, biggest_mrr, biggest_top3):
     argparse_dict = vars(args)
     with open(os.path.join(args.save_path_g2, 'config.json'), 'w') as fjson:
         json.dump(argparse_dict, fjson)
     with open(os.path.join(args.save_path_g2, 'final_test_score.json'), 'w') as scorejson:
         json.dump(biggest_score, scorejson)
+    with open(os.path.join(args.save_path_g2, 'final_test_mrr.json'), 'w') as mrrjson:
+        json.dump(biggest_mrr, mrrjson)
+    with open(os.path.join(args.save_path_g2, 'final_test_top3.json'), 'w') as top3json:
+        json.dump(biggest_top3, top3json)
     torch.save({
         # 'model_state_dict': model.state_dict(),
         'optimizer_state_dict': optimizer.state_dict()},
@@ -199,34 +203,145 @@ def load_data_and_pre_data(args):
     all_node_embedding = pre_data.embedding()  # 26989*200
     return target_train, target_train_small, target_test, target_test_big, all_node_embedding, data_G2, left_common, data_G1, en_index_G3_list_train_bef, en_index_G3_list_test_bef
 
+def evalua(args, out_test, sample_label_test, en_index_G3_list_test_bef, test_score_list, big_score, model, optimizer, test_mrr_list, big_mrr, acc, top3, big_top3, test_top3_list):
 
-def draw(args, test_score_list, train_loss_list):
+    # MRR
+    logging.info("calculate MRR... ...")
+    MRR_final = 0
+    for i in range(out_test.shape[0]):  # 1544
+        target_line = sample_label_test[i, :]
+        aim_top_pos = torch.nonzero(target_line)
+        aim_top_num = aim_top_pos.shape[0]
+        out_line = out_test[i, :]  # 150
+        argsort = torch.argsort(out_line, dim=0, descending=True)
+        print("for debug")
+        MRR_tmp = 0
+        for cur_tmp in aim_top_pos:
+            aim_top_pos_copy = aim_top_pos.squeeze(1)
+            t_list = aim_top_pos_copy.numpy().tolist()
+            t_i = cur_tmp.item()
+            t_list.remove(t_i)
+            argsort_list = argsort.numpy().tolist()
+            argsort_list_tmp = [i for i in argsort_list if i not in t_list]
+            argsort_tensor_tmp = torch.Tensor(argsort_list_tmp)
+            ranking_tmp = (argsort_tensor_tmp == cur_tmp).nonzero()
+            assert ranking_tmp.size(0) == 1
+            ranking_tmp = ranking_tmp.item() + 1
+            mrr_tmp = 1.0 / ranking_tmp
+            MRR_tmp = MRR_tmp + mrr_tmp
+        MRR_final_tmp = MRR_tmp / aim_top_num
+        MRR_final = MRR_final + MRR_final_tmp
+    MRR = MRR_final / out_test.shape[0]
+    test_mrr_list.append(MRR)
+    logging.info("MRR:{}".format(MRR))
+    if MRR > big_mrr:
+        logging.info("current MRR is bigger, before:{}, current:{}... ...".format(big_mrr, MRR))
+        big_mrr = MRR
+    else:
+        logging.info("biggest MRR:{} ... ".format(big_mrr))
+
+
+    # top3
+    logging.info("calculate Hit@3... ...")
+    for i in range(out_test.shape[0]):  # 1544
+        top3_temp = 0
+        out_line = out_test[i, :]  # 150
+        target_line = sample_label_test[i, :]
+        aim_top_pos = torch.nonzero(target_line)
+        # aim_top_num = aim_top_pos.shape[0]
+        # maxk = max((aim_top_num*10,))
+        # maxk = max((aim_top_num,))
+        out_top, out_top_pos = out_line.topk(3, 0, True, True)
+        for ii in out_top_pos:
+            if ii in aim_top_pos:
+                top3_temp = top3_temp + 1
+        top3 = top3_temp / 3 + top3
+    final_top3 = top3 / len(en_index_G3_list_test_bef)
+    test_top3_list.append(final_top3)
+    logging.info("top@3:{}".format(final_top3))
+    if final_top3 > big_top3:
+        logging.info(
+            "current top@3 is bigger, before:{}, current:{}... ...".format(big_top3, final_top3))
+        big_top3 = final_top3
+    else:
+        logging.info("biggest top@3:{} ... ".format(big_top3))
+
+    # accuracy
+    logging.info("calculate accuracy... ...")
+    for i in range(out_test.shape[0]):  # 1544
+        acc_temp = 0
+        out_line = out_test[i, :]  # 150
+        target_line = sample_label_test[i, :]
+        aim_top_pos = torch.nonzero(target_line)
+        aim_top_num = aim_top_pos.shape[0]
+        # maxk = max((aim_top_num*10,))
+        maxk = max((aim_top_num,))
+        out_top, out_top_pos = out_line.topk(maxk, 0, True, True)
+        for ii in out_top_pos:
+            if ii in aim_top_pos:
+                acc_temp = acc_temp + 1
+        acc = acc_temp / aim_top_num + acc
+    final_acc = acc / len(en_index_G3_list_test_bef)
+    test_score_list.append(final_acc)
+    logging.info("accuracy:{}".format(final_acc))
+    if final_acc > big_score:
+        logging.info(
+            "current accuracy is bigger, before:{}, current:{}... ...".format(big_score, final_acc))
+        big_score = final_acc
+        save_model(model, optimizer, args, big_score, big_mrr, big_top3)
+    else:
+        logging.info("biggest accuracy:{} ... ".format(big_score))
+
+    return test_score_list, big_score, test_mrr_list, big_mrr, test_top3_list, big_top3
+
+
+
+def draw(args, test_score_list, train_loss_list, test_mrr_list, test_top3_list):
     x1 = range(0, args.epoch)
     x2 = range(0, args.epoch)
+    x3 = range(0, args.epoch)
+    x4 = range(0, args.epoch)
     y1 = test_score_list
     y2 = train_loss_list
-    plt.subplot(2, 1, 1)
+    y3 = test_mrr_list
+    y4 = test_top3_list
+
+    plt.subplot(4, 1, 1)
     plt.plot(x1, y1, 'b*')
-    plt.title('Test score vs. epochs')
-    plt.ylabel('Test score')
-    plt.subplot(2, 1, 2)
+    plt.title('Accuracy vs. epochs')
+    plt.ylabel('Accuracy')
+    plt.subplot(4, 1, 2)
     plt.plot(x2, y2, 'b*')
     plt.xlabel('Train loss vs. epochs')
     plt.ylabel('Train loss')
-    plt.savefig(os.path.join(args.save_path_g2, 'Train_loss_and_Test_score_*.png'))
+    plt.subplot(4, 1, 3)
+    plt.plot(x3, y3, 'b*')
+    plt.xlabel('Test MRR vs. epochs')
+    plt.ylabel('Test MRR')
+    plt.subplot(4, 1, 4)
+    plt.plot(x4, y4, 'b*')
+    plt.xlabel('Test Top@3 vs. epochs')
+    plt.ylabel('Test Top@3')
+    plt.savefig(os.path.join(args.save_path_g2, 'Train_loss_MRR_Top@3_Accuracy_*.png'))
 
 
-    plt.subplot(2, 1, 1)
+    plt.subplot(4, 1, 1)
     plt.plot(x1, y1, 'b-')
-    plt.title('Test score vs. epochs')
-    plt.ylabel('Test score')
-    plt.subplot(2, 1, 2)
+    plt.title('Accuracy vs. epochs')
+    plt.ylabel('Accuracy')
+    plt.subplot(4, 1, 2)
     plt.plot(x2, y2, 'b-')
     plt.xlabel('Train loss vs. epochs')
     plt.ylabel('Train loss')
-    plt.savefig(os.path.join(args.save_path_g2, 'Train_loss_and_Test_score__.png'))
-
-
+    plt.subplot(4, 1, 3)
+    plt.plot(x3, y3, 'b-')
+    plt.xlabel('Test MRR vs. epochs')
+    plt.ylabel('Test MRR')
+    plt.subplot(4, 1, 4)
+    plt.plot(x4, y4, 'b-')
+    plt.xlabel('Test Top@3 vs. epochs')
+    plt.ylabel('Test Top@3')
+    plt.savefig(os.path.join(args.save_path_g2, 'Train_loss_MRR_Top@3_Accuracy__.png'))
 
 
 def main(args):
@@ -235,6 +350,8 @@ def main(args):
 
         train_loss_list = []
         test_score_list = []
+        test_mrr_list = []
+        test_top3_list = []
 
         logging.info("load data and pre-process data... ...")
         target_train, target_train_small, target_test, target_test_big, all_node_embedding, data_G2, left_common, data_G1, en_index_G3_list_train_bef, en_index_G3_list_test_bef = load_data_and_pre_data(args)
@@ -250,6 +367,12 @@ def main(args):
             with open(os.path.join(args.save_path_g2, 'final_test_score.json')) as fin:  # 26078
                 for line in fin:
                     big_score = float(line)
+            with open(os.path.join(args.save_path_g2, 'final_test_mrr.json')) as fin:  # 26078
+                for line in fin:
+                    big_mrr = float(line)
+            with open(os.path.join(args.save_path_g2, 'final_test_top3.json')) as fin:  # 26078
+                for line in fin:
+                    big_top3 = float(line)
             checkpoint = torch.load(os.path.join(args.save_path_g2, 'checkpoint'))
             model = torch.load(os.path.join(args.save_path_g2, 'model.pkl'))
             optimizer = torch.optim.Adam(model.parameters(), lr=args.learn_rate, weight_decay=0.0005)
@@ -257,6 +380,8 @@ def main(args):
         else:
             logging.info("Initialing model and optimizer... ...")
             big_score = 0
+            big_mrr = 0
+            big_top3 = 0
             model = Model(args, data_G2, all_node_embedding, left_common, target_train).to(device)
             optimizer = torch.optim.Adam(model.parameters(), lr=args.learn_rate, weight_decay=0.0005)
 
@@ -285,57 +410,24 @@ def main(args):
             optimizer.step()
             # print("after optimizer, the node_embedding:{}".format(torch.mean(model.all_node_embedding ** 2)))
 
-
             logging.info("test... ...")
             model.eval()
             acc = 0
+            top3 = 0
             out_test = model(args, data_G2.edge_index, data_G2.edge_type, data_G1.edge_index, en_index_G3_list_test_bef, sample_index_test, sample_index_min_test)  # 1544*150
 
-            # accuracy
-            logging.info("calculate accuracy... ...")
-            for i in range(out_test.shape[0]):  # 1544
-                acc_temp = 0
-                out_line = out_test[i,:]  # 150
-                target_line = sample_label_test[i,:]
-                aim_top_pos = torch.nonzero(target_line)
-                aim_top_num = aim_top_pos.shape[0]
-                # maxk = max((aim_top_num*10,))
-                maxk = max((aim_top_num,))
-                out_top, out_top_pos = out_line.topk(maxk, 0, True, True)
-                for ii in out_top_pos:
-                    if ii in aim_top_pos:
-                        acc_temp = acc_temp + 1
-                acc = acc_temp/aim_top_num + acc
-            final_acc = acc/len(en_index_G3_list_test_bef)
-            test_score_list.append(final_acc)
-            logging.info("accuracy:{}".format(final_acc))
-            if final_acc > big_score:
-                logging.info("current accuracy is bigger, before:{}, current:{}, save model... ...".format(big_score, final_acc))
-                big_score = final_acc
-                save_model(model, optimizer, args, big_score)
-            else:
-                logging.info("biggest accuracy:{} ... ".format(big_score))
-
-            # MRR
-            logging.info("calculate MRR... ...")
-
-
-
-
-
-
-
-
-
-
+            # evaluation
+            test_score_list, big_score, test_mrr_list, big_mrr, test_top3_list, big_top3 = evalua(args, out_test, sample_label_test, en_index_G3_list_test_bef, test_score_list, big_score, model, optimizer, test_mrr_list, big_mrr, acc, top3, big_top3, test_top3_list)
             end = datetime.datetime.now()
             logging.info("running time in epoch {} is {} seconds:".format(epoch, str((end - start).seconds)))
 
         logging.info("finished {} epochs... ...".format(args.epoch))
         logging.info("mean accuracy:{} for {} epochs... ...".format(np.mean(test_score_list), args.epoch))
         logging.info("biggest accuracy:{} ... for {} epochs... ...".format(big_score, args.epoch))
+        logging.info("mean MRR:{} for {} epochs... ...".format(np.mean(test_mrr_list), args.epoch))
+        logging.info("biggest MRR:{} ... for {} epochs... ...".format(big_mrr, args.epoch))
         logging.info("ploting... ...")
-        draw(args, test_score_list, train_loss_list)
+        draw(args, test_score_list, train_loss_list, test_mrr_list, test_top3_list)
         logging.info("_________________ finished {} epochs _________________ ".format(args.epoch))
 
 
